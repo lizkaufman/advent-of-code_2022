38dffb2fe7bed492849546345e284d94a8ccebe7
Liz Kaufman <lizkaufman92@gmail.com>
2022-12-04 21:47:11 +0000

    4 dec pt2

--- 04-dec/index.js
+++ 04-dec/index.js
@@ -25,3 +25,44 @@ function hasFullyContainedRange(pair) {
     (secondStart >= firstStart && secondFinish <= firstFinish)
   );
 }
+
+//Part 2:
+
+const solution_pt2 = countOverlappedPairs(data);
+console.log({ solution_pt2 });
+
+function countOverlappedPairs(data) {
+  let count = 0;
+  data.forEach((pair) => {
+    if (hasOverlap(pair)) {
+      count += 1;
+    }
+  });
+  return count;
+}
+
+function hasOverlap(pair) {
+  let result = false;
+
+  const rangeBounds = pair.split(",");
+  const [firstStart, firstFinish] = rangeBounds[0].split("-");
+  const [secondStart, secondFinish] = rangeBounds[1].split("-");
+  const firstRange = populateRange(firstStart, firstFinish);
+  const secondRange = populateRange(secondStart, secondFinish);
+
+  firstRange.forEach((num) => {
+    if (secondRange.includes(num)) {
+      result = true;
+    }
+  });
+
+  return result;
+}
+
+function populateRange(start, end) {
+  const range = [];
+  for (let i = Number(start); i <= end; i++) {
+    range.push(i);
+  }
+  return range;
+}
