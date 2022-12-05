f587da8a81e81da846c06a6e23ee0f72afa210a2
Liz Kaufman <lizkaufman92@gmail.com>
2022-12-05 18:37:51 +0000

    5 dec pt2

--- 05-dec/index.js
+++ 05-dec/index.js
@@ -1,6 +1,6 @@
 import data from "./input.js";
 
-const stacks = {
+const stacks_pt1 = {
   1: ["T", "Z", "B"],
   2: ["N", "D", "T", "H", "V"],
   3: ["D", "M", "F", "B"],
@@ -12,32 +12,16 @@ const stacks = {
   9: ["S", "D", "F", "L", "Q", "M"],
 };
 
-const testData = [
-  "move 1 from 7 to 4",
-  "move 1 from 6 to 2",
-  "move 5 from 9 to 4",
-  "move 2 from 2 to 8",
-  "move 2 from 2 to 6",
-  "move 3 from 3 to 7",
-  "move 3 from 7 to 1",
-  "move 1 from 9 to 4",
-  "move 4 from 7 to 3",
-  "move 5 from 1 to 8",
-  "move 1 from 1 to 2",
-  "move 1 from 4 to 9",
-  "move 4 from 5 to 6",
-  "move 1 from 5 to 8",
-  "move 3 from 2 to 4",
-];
+//Part 1:
 
 moveCratesWithMovesList(data);
-const solution_pt1 = getTopsOfStacks(stacks);
+const solution_pt1 = getTopsOfstacks(stacks_pt1);
 console.log({ solution_pt1 });
 
-function getTopsOfStacks(finalStacks) {
+function getTopsOfstacks(finalstacks) {
   let result = "";
-  for (const stack in finalStacks) {
-    result += finalStacks[stack][0];
+  for (const stack in finalstacks) {
+    result += finalstacks[stack][0];
   }
   return result;
 }
@@ -45,7 +29,7 @@ function getTopsOfStacks(finalStacks) {
 function moveCratesWithMovesList(data) {
   data.forEach((move) => {
     const [numberToMove, start, end] = extractMoveNumbers(move);
-    makeMove(numberToMove, start, end);
+    makeMoveOneAtATime(numberToMove, start, end);
   });
 }
 
@@ -54,9 +38,42 @@ function extractMoveNumbers(move) {
   return [moveWords[1], moveWords[3], moveWords[5]];
 }
 
-function makeMove(numberToMove, start, end) {
+function makeMoveOneAtATime(numberToMove, start, end) {
   for (let i = 0; i < numberToMove; i++) {
-    const crate = stacks[start].shift();
-    stacks[end].unshift(crate);
+    const crate = stacks_pt1[start].shift();
+    stacks_pt1[end].unshift(crate);
   }
+}
+
+//Part 2:
+
+const stacks_pt2 = {
+  1: ["T", "Z", "B"],
+  2: ["N", "D", "T", "H", "V"],
+  3: ["D", "M", "F", "B"],
+  4: ["L", "Q", "V", "W", "G", "J", "T"],
+  5: ["M", "Q", "F", "V", "P", "G", "D", "W"],
+  6: ["S", "F", "H", "G", "Q", "Z", "V"],
+  7: ["W", "C", "T", "L", "R", "N", "S", "Z"],
+  8: ["M", "R", "N", "J", "D", "W", "H", "Z"],
+  9: ["S", "D", "F", "L", "Q", "M"],
+};
+
+moveCratesWithMovesList9001(data);
+const solution_pt2 = getTopsOfstacks(stacks_pt2);
+console.log({ solution_pt2 });
+
+function moveCratesWithMovesList9001(data) {
+  data.forEach((move) => {
+    const [numberToMove, start, end] = extractMoveNumbers(move);
+    makeMoveAllAtOnce(numberToMove, start, end);
+  });
+}
+
+function makeMoveAllAtOnce(numberToMove, start, end) {
+  const cratesToMove = stacks_pt2[start].slice(0, numberToMove);
+  for (let i = 0; i < numberToMove; i++) {
+    stacks_pt2[start].shift();
+  }
+  stacks_pt2[end] = [...cratesToMove, ...stacks_pt2[end]];
 }
